bbe56ea3918d1c1c312d549e92b0c75d9c9a5d6a
anton11nahhas <antonna@edu.hac.ac.il>
2023-07-26 16:16:52 +0300

    improved code design and readablity, provided docs as well.

--- checkpoint.c
+++ checkpoint.c
@@ -2,12 +2,25 @@
 #include <string.h>
 #include <stdlib.h>
 
+//-------------CONSTS----------------------------
 #define COURSES 10
 #define NAMELEN 15
 #define LEVELS 7
 #define CLASSES 20
 #define CELLLEN 13
+#define LINESIZE 256
+#define READMODE "r"
+#define WRITEBINARYMODE "wb"
+#define FILENAME "students_with_class.txt"
+#define OPENFILEERR "Error openning file..\n"
+#define WRITETOFILEERR "Error opening file for writing.\n"
+#define BINARYFILE "school_data.bin"
 
+//------------STRUCT SECTION-----------------------
+
+//Student struct that stores all data of the user, first name, last name,
+//cellphone number, level, class, and grades. In addtition theres a pointer
+//of type student that points to the next student in the linked list.
 struct student {
     char first_name[NAMELEN];
     char last_name[NAMELEN];
@@ -18,25 +31,38 @@ struct student {
     struct student* next;
 };
 
+//School struct represents a dynamic linked list of students, to identify
+//a student use the level he is in and the class he attends.
 struct school {
     struct student* DB[LEVELS][CLASSES];
 };
 
 static struct school S;
 
+
+//---------------FUNCTION DECLARATIONS-----------------
 void init_db();
 void save_to_binary(const char* filename);
 void display_first_10_students();
 
 
+//---------------FUNCTION SECTION----------------------
+/*
+    This function opens the file of data in read mode, checks if it was succesffully 
+    opened if not displays an error message. 
+    Then the function reads each line from the file, extracts each input according to
+    its type, and saved it in our data structure under the name of newStudent, 
+    (which was allocated dynamically). Then accordingly save the student at the
+    end of the linked list. Finally the save the file in binary, and close it. 
+*/
 void init_db() {
-    FILE* file = fopen("students_with_class.txt", "r");
+    FILE* file = fopen(FILENAME, READMODE);
         if (file == NULL) {
-        printf("Error opening file.\n");
+        printf(OPENFILEERR);
         return;
     }
 
-    char buffer[256];
+    char buffer[LINESIZE];
 
     while (fgets(buffer, sizeof(buffer), file) != NULL) {
         struct student* newStudent = (struct student*)malloc(sizeof(struct student));
@@ -67,11 +93,17 @@ void init_db() {
         }
     }
 
-    save_to_binary("school_data.bin");
+    save_to_binary(BINARYFILE);
 
     fclose(file);
 }
 
+
+/*This function displays the first 10 students from the struct, this is done
+after parsing the data from the file and populating it onto the data structure.
+the function does so by looping through the levels and classes in the linked list.
+This is done until the counter reaches 10, becuase we are only interested in the 
+first ten users, basically this function is used for debug purposes*/
 void display_first_10_students() {
     int count = 0;
 
@@ -107,11 +139,15 @@ void display_first_10_students() {
     }
 }
 
+/**
+ This function receives a file name, opens it in write binary mode, and writes to
+ the data from our data structure.
+*/
 void save_to_binary(const char* filename) {
-    FILE* file = fopen(filename, "wb");
+    FILE* file = fopen(filename, WRITEBINARYMODE);
 
     if (file == NULL) {
-        printf("Error opening file for writing.\n");
+        printf(WRITETOFILEERR);
         return;
     }
 
@@ -120,6 +156,7 @@ void save_to_binary(const char* filename) {
     fclose(file);
 }
 
+/*Main function*/
 int main(){
     init_db();
 
